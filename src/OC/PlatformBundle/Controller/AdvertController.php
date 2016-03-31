a7a36296e3eab35de3c044ea3e6a83c3945207e5
xablen <xavier.parfait@gmail.com>
2016-03-31 16:24:01 +0200

    modification mineures

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -9,12 +9,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
-    // On récupère tous les paramètres en arguments de la méthode
-    public function viewSlugAction($slug, $year, $_format)
+    public function indexAction()
     {
-        return new Response(
-            "On pourrait afficher l'annonce correspondant au
-            slug '".$slug."', créée en ".$year." et au format ".$_format."."
+        // On veut avoir l'URL de l'annonce d'id 5.
+        $url = $this->get('router')->generate(
+            'oc_platform_view', // 1er argument : le nom de la route
+            array('id' => 5)    // 2e argument : les valeurs des paramètres
         );
+        // $url vaut « /platform/advert/5 »
+
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
     }
 }
